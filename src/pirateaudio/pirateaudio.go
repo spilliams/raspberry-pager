e880bb054c6deb767d67fb991d64ac0c9392b0ac
Spencer Williams <s@spencerenglish.com>
2022-04-03 19:37:59 -0700

    first crack at code

---
+++ src/pirateaudio/pirateaudio.go
@@ -0,0 +1,3 @@
+// Package pirateaudio provides interfaces to configure and control the various
+// Pirate Audio boards available from Pimoroni. https://pimoroni.com/pirateaudio
+package pirateaudio
---
+++ src/pirateaudio/piratespeaker/piratespeaker.go
@@ -0,0 +1,7 @@
+// Package piratespeaker provides interfaces to configure and control the
+// Pirate Audio: Speaker board available from Pimoroni.
+// https://pimoroni.com/pirateaudio
+// The board features a 1.3" square TFT screen, 4 buttons, and a 1W 8ohm speaker.
+// Note that to use this package you will need to configure your Raspberry Pi
+// a little. See https://github.com/pimoroni/pirate-audio#installation.
+package piratespeaker
