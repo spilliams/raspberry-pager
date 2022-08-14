b3ed85a395b9fc620f3cacea90ce6774d45d3257
Spencer Williams <s@spencerenglish.com>
2022-08-14 08:53:35 -0700

    a different lib

--- agent/examples/speaker.py
+++ agent/examples/speaker.py
@@ -1,18 +1,29 @@
-import sounddevice
+# import sounddevice
 
 
-def play_clip(filename="WEEEOOOOWW.mp3"):
-    out_device = "default"
-    out_stream = sounddevice.OutputStream(
-        device=out_device,
-        dtype="int16",
-        channels=2,
-        samplerate=16000,
-        callback=self.audio_playback_callback,
-    )
+# def play_clip(filename="WEEEOOOOWW.mp3"):
+#     out_device = "default"
+#     out_stream = sounddevice.OutputStream(
+#         device=out_device,
+#         dtype="int16",
+#         channels=2,
+#         samplerate=16000,
+#         callback=self.audio_playback_callback,
+#     )
 
-    out_stream.start()
+#     out_stream.start()
 
 
-if __name__ == "__main__":
-    play_clip()
+# if __name__ == "__main__":
+#     play_clip()
+
+
+import pygame
+
+path = "/home/pi/pager/source/agent/examples/WEEEOOOOWW.mp3"
+pygame.mixer.init()
+pygame.mixer.music.set_volume(0.7)
+pygame.mixer.music.load(path)
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+    pass
