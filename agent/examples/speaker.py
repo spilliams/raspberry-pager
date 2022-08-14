251d9a3d9e90eb1dbc19b6b13dce16b3ac8f1486
Spencer Williams <s@spencerenglish.com>
2022-08-14 08:55:59 -0700

    keep to builtins if we can

--- agent/examples/speaker.py
+++ agent/examples/speaker.py
@@ -1,29 +1,29 @@
-# import sounddevice
+import sounddevice
 
 
-# def play_clip(filename="WEEEOOOOWW.mp3"):
-#     out_device = "default"
-#     out_stream = sounddevice.OutputStream(
-#         device=out_device,
-#         dtype="int16",
-#         channels=2,
-#         samplerate=16000,
-#         callback=self.audio_playback_callback,
-#     )
+def play_clip(filename="WEEEOOOOWW.mp3"):
+    out_device = "default"
+    out_stream = sounddevice.OutputStream(
+        device=out_device,
+        dtype="int16",
+        channels=2,
+        samplerate=22000,
+        callback=self.audio_playback_callback,
+    )
 
-#     out_stream.start()
-
-
-# if __name__ == "__main__":
-#     play_clip()
+    out_stream.start()
 
 
-import pygame
+if __name__ == "__main__":
+    play_clip()
 
-path = "/home/pi/pager/source/agent/examples/WEEEOOOOWW.mp3"
-pygame.mixer.init()
-pygame.mixer.music.set_volume(0.7)
-pygame.mixer.music.load(path)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    pass
+
+# import pygame
+
+# path = "/home/pi/pager/source/agent/examples/WEEEOOOOWW.mp3"
+# pygame.mixer.init()
+# pygame.mixer.music.set_volume(0.7)
+# pygame.mixer.music.load(path)
+# pygame.mixer.music.play()
+# while pygame.mixer.music.get_busy():
+#     pass
